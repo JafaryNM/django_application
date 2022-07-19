55512fdd9a0279b24143f385e0ce93f8a1111586
JafaryNm <jaffarymdegela@gmail.com>
2022-07-19 18:15:13 +0300

    Update Filter

--- myapp/views.py
+++ myapp/views.py
@@ -9,6 +9,7 @@ def  example_view(request):
 def variable_view(request):
     # Dictionary Variable should be taken out 
     # Passing data to the template using context
+    # Building template tag https://docs.djangoproject.com/en/4.0/ref/templates/builtins/ references
     
     my_variable={'first_name':'john', 'second_name':'lemar',
                  'somelist':[90,30,20], 'my_dic':{'inside_key':'inside_value'}
