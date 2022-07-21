85c414bff9c5eeb76285e25223d6347f4c4c875b
JafaryNm <jaffarymdegela@gmail.com>
2022-07-21 16:06:51 +0300

    Patient Model Updated

--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,6 @@ class Patient(models.Model):
     first_name= models.CharField(max_length = 150)
     last_name=models.CharField(max_length=150)
     age=models.IntegerField()
+    def __str__(self):
+        
+        return f"{self.first_name},{self.last_name}"
