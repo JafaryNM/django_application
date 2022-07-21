f7577b270be6eb0451a68753478bd19f7d18c7d9
JafaryNm <jaffarymdegela@gmail.com>
2022-07-21 16:47:44 +0300

    Data Intergration and Update

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 
@@ -6,7 +7,9 @@ from django.db import models
 class Patient(models.Model):
     first_name= models.CharField(max_length = 150)
     last_name=models.CharField(max_length=150)
-    age=models.IntegerField()
+    age=models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    heartrate=models.IntegerField(default=60, validators=[MinValueValidator(0), MaxValueValidator(60)])
+    
     def __str__(self):
         
         return f"{self.first_name},{self.last_name}"
